0a4aed332a322a0ac7b7311816fb99504e20cf0e
houxxam <houxxam95@gmail.com>
2025-03-14 01:19:41 +0000

    updated

--- script/app.js
+++ script/app.js
@@ -28,21 +28,35 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
     const imgsContainer = document.querySelector('.imgs');
 
-    for (let i = 1; i < images.length; i++) {
+    for (let i = 0; i < images.length; i++) {
         let img = document.createElement('img');
         img.src = "imgs/" + images[i].img;
         img.alt = images[i].title;
-        img.className = "tumbnail w-1/7 cursor-pointer hover:opacity-100 opacity-70 ease-in transition-all duration-250";
+        img.className = "tumbnail w-1/8 cursor-pointer hover:opacity-100 opacity-70 ease-in transition-all duration-250";
         imgsContainer.appendChild(img);
         img.addEventListener('click', () => setActiveImage(i));
+        
     }
     autoPlay();
 });
 
 function setActiveImage(index) {
+    let imgs = document.querySelectorAll('.imgs img');
     currentSlide = index;
     active.src = "imgs/" + images[currentSlide].img;
     imgTitle.textContent = images[currentSlide].title;
+    console.log(index);
+    imgs.forEach(img => {
+        img.classList.remove('opacity-100'); // Remove active class
+        img.classList.add('opacity-70'); // Set opacity to 70 for all
+    });
+    if(imgs[index]){
+        
+        imgs[index].classList.add('opacity-100');
+        console.log('Images:', imgs); // Check all the images
+        console.log('Active image at index:', index);
+    }
+    
 }
 
 // Function to go to the next slide
@@ -62,7 +76,7 @@ nextBtn.addEventListener('click', goToNextSlide);
 prevBtn.addEventListener('click', goToPrevSlide);
 
 function autoPlay(){
-    intervalId = setInterval(goToNextSlide,5000);
+    intervalId = setInterval(goToNextSlide,3000);
 }
 
 function pauseAutoPlay() {
